158846a29a086d158a88736abdb2305f8e98d230
KotonoSora <reply@kotonosora.com>
2022-10-29 17:58:51 +0700

    init project

---
+++ familytree/familytreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  familytreeApp.swift
+//  familytree
+//
+//  Created by Thang Nguyen on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct familytreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
